686de00e8b50fbc78f3c71c4b63543b42e916a71
Guilherme Carlos <grcarlos.dev@gmail.com>
2025-06-26 23:12:42 -0300

    preventDefault feita

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const amount = document.getElementById("amount");
+const currency = document.getElementById("currency")
 
-
+currency.addEventListener("submit", (event) => {
+  event.preventDefault()
+})
 
 // Manipula o input amount para receber somente números.
 amount.addEventListener("input", () => {
