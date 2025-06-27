f11f91f35c097215940c0ccdc247799a9ddea560
Guilherme Carlos <grcarlos.dev@gmail.com>
2025-06-26 23:15:54 -0300

    correção no envio do formulario

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const amount = document.getElementById("amount");
 const currency = document.getElementById("currency")
+const form = document.querySelector("form")
 
-currency.addEventListener("submit", (event) => {
+form.onsumit = (event) => {
   event.preventDefault()
-})
+}
 
 // Manipula o input amount para receber somente números.
 amount.addEventListener("input", () => {
