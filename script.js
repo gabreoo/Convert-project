0be681fce2e80b852b362fb7714409e565076b8b
Guilherme Carlos <grcarlos.dev@gmail.com>
2025-06-26 23:31:10 -0300

    erro de digitação corrigido

--- script.js
+++ script.js
@@ -2,13 +2,16 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency")
 const form = document.querySelector("form")
 
-form.onsumit = (event) => {
-  event.preventDefault()
-}
 
 // Manipula o input amount para receber somente números.
 amount.addEventListener("input", () => {
   const hasCharacterRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharacterRegex, "");
-  console.log(amount.value)
 });
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+  console.log(currency.value)
+}
+
+ 
