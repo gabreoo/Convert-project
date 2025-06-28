bba7ea10302874a4b757ddc49c5d022147e2f7ee
Guilherme Carlos <grcarlos.dev@gmail.com>
2025-06-27 23:16:54 -0300

    implementado o valor da cotação do dia

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-const amount = document.getElementById("amount");
-const currency = document.getElementById("currency");
-const form = document.querySelector("form");
-
 // Definindo o valor da cotação
 const USD = 5.59;
 const GBP = 7.52;
 const EUR = 6.43;
 
+const amount = document.getElementById("amount");
+const currency = document.getElementById("currency");
+const form = document.querySelector("form");
+const footer = document.querySelector("main footer");
+const description = document.getElementById("description");
+
 // Manipula o input amount para receber somente números.
 amount.addEventListener("input", () => {
   const hasCharacterRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharacterRegex, "");
+});
+
+// capturando qual moeda vai ser usada
+form.onsubmit = (event) => {
+  event.preventDefault();
 
   switch (currency.value) {
     case "USD":
@@ -23,13 +30,15 @@ amount.addEventListener("input", () => {
       convertCurrency(amount.value, GBP, "£");
       break;
   }
-});
-
-// capturando qual moeda vai ser usada
-form.onsubmit = (event) => {
-  event.preventDefault();
 };
 
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol)
+  try {
+    footer.classList.add("show-result");
+    description.textContent = `${symbol} 1 = ${price}`
+  } catch (error) {
+    footer.classList.remove("show-result");
+    alert("Algo saiu errado, por favor tente novamente em alguns instantes");
+    console.log(error);
+  }
 }
